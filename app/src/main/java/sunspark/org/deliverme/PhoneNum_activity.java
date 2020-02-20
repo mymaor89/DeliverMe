7b097f2e35426fbd11c878c3909c692feba82433
Shalev <shalev01245@gmail.com>
2020-02-20 20:59:34 +0200

    added and styled phone number phone

---
+++ app/src/main/java/sunspark/org/deliverme/PhoneNum_activity.java
@@ -0,0 +1,13 @@
+package sunspark.org.deliverme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneNum_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_num_activity);
+    }
+}
