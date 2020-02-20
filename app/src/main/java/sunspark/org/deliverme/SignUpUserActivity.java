d989933986e392a3ba2af98ca188f13f171a619e
reznik1024 <ron.reznikov@gmail.com>
2020-02-20 21:02:22 +0200

    start of sign up as a user

---
+++ app/src/main/java/sunspark/org/deliverme/SignUpUserActivity.java
@@ -0,0 +1,37 @@
+package sunspark.org.deliverme;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class SignUpUserActivity extends AppCompatActivity {
+   EditText firstName, lastName;
+    ImageView profilePic;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        firstName =(EditText)findViewById(R.id.firstName);
+        lastName =(EditText)findViewById(R.id.lastName);
+        profilePic = (ImageView) findViewById(R.id.image);
+
+    }
+
+}
