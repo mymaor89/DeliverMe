da665703e835316606df0f38b1459718c2265175
Ben Bashvitz <bashvitzben@gmail.com>
2020-02-21 08:22:21 +0200

    added an activity that shows the request the deliverer sees.

--- app/src/main/java/sunspark/org/deliverme/MainActivity.java
+++ app/src/main/java/sunspark/org/deliverme/MainActivity.java
@@ -1,10 +1,11 @@
 package sunspark.org.deliverme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
