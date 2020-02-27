8d7097fb99bf9c2e8014fe8b1bf6a6823296ae59
reznik1024 <ron.reznikov@gmail.com>
2020-02-27 20:27:38 +0200

    changed the request and added premisions

--- app/src/main/java/sunspark/org/deliverme/SignUpUserActivity.java
+++ app/src/main/java/sunspark/org/deliverme/SignUpUserActivity.java
@@ -6,32 +6,30 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 public class SignUpUserActivity extends AppCompatActivity {
-   EditText firstName, lastName;
+   EditText firstName, lastName,email;
     ImageView profilePic;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_requests_page);
 
         firstName =(EditText)findViewById(R.id.firstName);
         lastName =(EditText)findViewById(R.id.lastName);
         profilePic = (ImageView) findViewById(R.id.image);
+        email= (EditText) findViewById(R.id.Email);
 
+        button =  (Button) findViewById(R.id.signUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
