4b4eb92382a9c0b738771fff35d41b60f2f9f819
maor yatskan <mymaor89@gmail.com>
2020-03-05 19:51:17 +0200

    menu

--- app/src/main/java/sunspark/org/deliverme/MainActivity.java
+++ app/src/main/java/sunspark/org/deliverme/MainActivity.java
@@ -7,12 +7,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     Intent i;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    //    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle item selection
+//        switch (item.getItemId()) {
+//            case R.id.new_game:
+//                newGame();
+//                return true;
+//            case R.id.help:
+//                showHelp();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
         myRef.setValue("Hi, World!");
-
         Intent resultIntent = new Intent(this,R.layout.class);
         PendingIntent resultPendingIntent =
                 PendingIntent.getActivity(
